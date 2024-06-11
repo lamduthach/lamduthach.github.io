803b5c935e6ff75082f99cacc3ef6057d4c1eac0
user <Lam Du Thach>
2024-06-12 00:00:10 +0900

    update page

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,4 @@
-/*!
-* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
 window.addEventListener('DOMContentLoaded', event => {
-
     // Activate Bootstrap scrollspy on the main nav element
     const sideNav = document.body.querySelector('#sideNav');
     if (sideNav) {
@@ -17,7 +7,6 @@ window.addEventListener('DOMContentLoaded', event => {
             rootMargin: '0px 0px -40%',
         });
     };
-
     // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
     const responsiveNavItems = [].slice.call(
@@ -33,21 +22,6 @@ window.addEventListener('DOMContentLoaded', event => {
 
 });
 
-var download = function (file) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            var element = document.createElement('a');
-            element.setAttribute('href', document.location.origin + document.location.pathname + "resource/" + file);
-            element.setAttribute('target', "_blank");
-            document.body.appendChild(element);
-            element.click();
-            document.body.removeChild(element);
-            resolve(true)
-        }, 300);
-    });
-}
-
-var downloadCVs = function () {
-    download("LamDuThach_CV_EN.xlsx")
-    .then(() => {download("LamDuThach_CV_VN.xlsx"); });
-}
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+});
