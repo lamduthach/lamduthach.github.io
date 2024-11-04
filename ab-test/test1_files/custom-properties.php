5f8f4400aa24e8b30434422b1613578233522ac4
Lam Du Thach <lamduthach@fabercompany.co.jp>
2024-11-04 10:18:53 +0700

    ab test

---
+++ ab-test/test1_files/custom-properties.php
@@ -0,0 +1,10 @@
+
+body {
+  --parts-max-width: 640px;
+  --main-max-width: 680.85106382979px;
+  --contents-max-width: 950.85106382979px;
+  --site-font-size: 16px;
+  --site-line-spacing: 1.6;
+  --site-character-spacing: 0.00em;
+  --parts-margin: 32px;
+}
